91ceca6f9c79ecae31d673d1e4d4dcfc0bd82ebf
Philip Heinser <philipheinser@me.com>
2013-04-06 01:13:45 +0200

    Initial Commit

---
+++ DigitalOceanManager/DOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DOAppDelegate.h
+//  DigitalOceanManager
+//
+//  Created by Philip Heinser on 06.04.13.
+//  Copyright (c) 2013 Philip Heinser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DigitalOceanManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DigitalOceanManager
+//
+//  Created by Philip Heinser on 06.04.13.
+//  Copyright (c) 2013 Philip Heinser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DOAppDelegate class]));
+    }
+}
